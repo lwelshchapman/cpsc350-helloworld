029b6d1cef5f55be41d73db34442d6ab9cd83bd8
lwelshchapman <lwelsh@chapman.edu>
2020-02-11 19:18:06 +0000

    Added helloworld.cpp

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # cpsc350-helloworld
+
+Hello World!
---
+++ helloworld.cpp
@@ -0,0 +1,20 @@
+/*
+ * Logan Welsh
+ * lwelsh@chapman.edu
+ * CPSC-350-01
+ * cpsc350-helloworld
+ * helloworld.cpp
+ */
+
+
+#include <iostream>
+
+using namespace std;
+
+
+int main(int argc, char** argv) {
+	
+	cout << "Hello World!" << endl;
+	
+	return 0;
+}
